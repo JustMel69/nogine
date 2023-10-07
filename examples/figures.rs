850a20332665f54d85037cfd9a071ae69fcbc6e6
Maestro Yowax <yowax25yt@gmail.com>
2023-10-07 15:43:21 +0200

    fix: Name not updated

--- examples/figures.rs
+++ examples/figures.rs
@@ -2,7 +2,7 @@ use nogine::{graphics::Graphics, window::WindowCfg, color::{Color4, Color}, math
 
 fn main() {
     // Create Window
-    let mut window = WindowCfg::default().main(true).res((1280, 720)).title("White Square Example").mode(nogine::window::WindowMode::Windowed).init().expect("Couldn't open window");
+    let mut window = WindowCfg::default().main(true).res((1280, 720)).title("Figures Example").mode(nogine::window::WindowMode::Windowed).init().expect("Couldn't open window");
 
     // Setup graphics
     Graphics::set_cam(Vector2::ZERO, 1.5);
